f73942b24d41612725f3eead4ba6157329ae06da
Patrick Reilly <Reilly-P6@ulster.ac.uk>
2025-09-28 22:20:08 +0100

    Improved program code.

--- BankingApp/Program.cs
+++ BankingApp/Program.cs
@@ -8,7 +8,9 @@ void BuildBankingApp()
 {
     //Display the main menu screen. 
     int choice;
+    int depositOrWithdraw;
     double depositAmount;
+    double withdrawAmount;
     double accountBalance = 1000.00;
 
     Console.WriteLine("Please choose an option");
@@ -21,7 +23,7 @@ void BuildBankingApp()
     //Convert the string to an integer.
     //Using explicit typecasting.
 
-    choice = Convert .ToInt32(Console.ReadLine());
+    choice = Convert.ToInt32(Console.ReadLine());
 
     //The || in the loop represents OR 
     //Which means that a menu choice of 1 OR 2 
@@ -33,14 +35,47 @@ void BuildBankingApp()
         {
             //Deposit or withdraw funds 
             Console.WriteLine("You have chosen to deposit or withdraw funds");
-            Console.WriteLine("How much do you want to deposit?");
-            Console.WriteLine("Enter the amount including pence using a decimal point");
+            Console.WriteLine("Please choose an option");
+            Console.WriteLine("3 - deposit");
+            Console.WriteLine("4 - withdraw");
+            Console.WriteLine("Please choose an option (3-4)");
+            depositOrWithdraw = Convert.ToInt32(Console.ReadLine());
 
-            depositAmount = Convert .ToDouble(Console.ReadLine());
-            Console.WriteLine($"Your original account balance was {accountBalance}");
+            if (depositOrWithdraw == 3)
+            {
+                Console.WriteLine("How much do you want to deposit?");
+                Console.WriteLine("Enter the amount including pence using a decimal point");
 
-            accountBalance += depositAmount;
-            Console.WriteLine($"Your new account balance is {accountBalance}");
+                depositAmount = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine($"Your original account balance was {accountBalance}");
+
+                accountBalance += depositAmount;
+                Console.WriteLine($"Your new account balance is {accountBalance}");
+            }
+            else if (depositOrWithdraw == 4)
+            {
+                Console.WriteLine("How much do you want to withdraw?");
+                Console.WriteLine("Enter the amount including pence using a decimal point");
+
+                withdrawAmount = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine($"Your original account balance was {accountBalance}");
+
+                if (accountBalance > withdrawAmount)
+                {
+                    accountBalance -= withdrawAmount;
+                    Console.WriteLine($"Your new account balance is {accountBalance}");
+                }
+                else if (withdrawAmount > accountBalance)
+                { 
+                    Console.WriteLine("You have insufficient funds to withdraw this amount");
+                }
+                 
+            }
+            else
+            {
+                //Invalid menu choice
+                Console.WriteLine("Invalid choice, please choose 3 or 4");
+            }
         }
         if (choice == 2)
         {
